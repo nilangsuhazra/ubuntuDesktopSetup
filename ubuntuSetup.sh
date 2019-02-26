6d6834b9137aea176bd6a818ce42ade46ec647d8
Nilangsu Hazra <nilangsuhazra@gmail.com>
2019-02-26 09:44:10 +0530

    added new packages for installation

--- ubuntuSetup.sh
+++ ubuntuSetup.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
-
-cd ~
-
+# ----------------------INSTALLTION INSTRUCTION----------------------
+# step01: open terminal(Ctrl+Alt+T)
+# step02: navigate to the directory where the ubuntuSetup.sh located
+# step03: make ubuntuSetup.sh file executable;
+#         chmod 755 ubuntuSetup.sh
+# step04: execute ubuntuSetup.sh file;
+#         ./ubuntuSetup.sh
+# -------------------------------------------------------------------
+# update and install latest ubuntu packages
 sudo apt update -y
+sudo apt upgrade -y
+# grab and install all packages
+sudo apt install $(cat ubuntu.packages) -y
